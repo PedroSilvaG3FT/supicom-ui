c6a305b672d07561de26e2808f5a6331176fa0fb
Pedro Silva <pedro.silva-dev@hotmail.com>
2024-09-17 16:50:31 -0300

    feat: locale documentation link

--- src/app/doc/features/internationalization/page.tsx
+++ src/app/doc/features/internationalization/page.tsx
@@ -1,6 +1,7 @@
 import { useTranslations } from "next-intl";
 import { Separator } from "@/_core/components/fragments/separator";
 import AppToggleLocale from "@/_shared/components/locale/app-toggle-locale";
+import { LinkPreview } from "@/_core/components/fragments/ui/link-preview";
 
 export default function InternationalizationPage() {
   const t = useTranslations();
@@ -11,6 +12,13 @@ export default function InternationalizationPage() {
 
       <Separator className="my-4" />
       <AppToggleLocale mode="inline" className="mb-4" />
+
+      <LinkPreview
+        url="https://next-intl-docs.vercel.app/docs/getting-started/app-router/without-i18n-routing"
+        className="font-semibold underline"
+      >
+        Complete guide
+      </LinkPreview>
 
       <h2 className="font-semibold">{t("example.title")}</h2>
       <h4 className="mb-5">{t("example.subtitle")}</h4>
