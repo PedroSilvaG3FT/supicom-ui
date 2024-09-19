fa5130fef0a14053ab22b67fa55967dec7699426
Pedro Silva <pedro.silva-dev@hotmail.com>
2024-09-19 01:33:56 -0300

    feat: hero modal height

--- src/app/portal/_sections/hero.tsx
+++ src/app/portal/_sections/hero.tsx
@@ -26,7 +26,7 @@ export default function PortalHero() {
   ];
 
   return (
-    <AuroraBackground className="shadow-md p-4 bg-black text-white border-zinc-200 h-[40rem] mobile:h-[84dvh] overflow-hidden">
+    <AuroraBackground className="shadow-md p-4 bg-black text-white border-zinc-200 h-[40rem] mobile:h-[92dvh] overflow-hidden">
       <section className="app-container pt-24 mobile:px-0">
         <section className="h-full w-full grid grid-cols-2 mobile:grid-cols-1">
           <div className="h-full flex flex-col justify-center mobile:items-center">
